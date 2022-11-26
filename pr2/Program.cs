5f34e907bff5107b5256da349fe7c11f541f8d02
Keith <4restrei@gmail.com>
2022-11-26 09:24:49 +0300

    asdf

---
+++ pr2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Random rnd = new Random();
+Console.WriteLine("Введите число, которое хотите найти");
+int number = Convert.ToInt32(Console.ReadLine());
+int[,] numbersArray = new int [10,10];
+FillArrayRandomNumbers(numbersArray);
+PrintArray(numbersArray);
+if (CheckNumber(number) == numbersArray[10,10])
+{
+    Console.WriteLine("Такого числа в массиве нет");
+}
+
+
+int CheckNumber(int number)
+{
+    for (int i = 0; i < numbersArray.GetLength(1); i++)
+    {
+        for (int j = 0; j < numbersArray.GetLength(0);j++)
+        {
+            if (number == numbersArray[j,i]) 
+            {
+                Console.WriteLine ($"Число находиться в позиции: ");
+                break;
+                return numbersArray[j,i];
+            }
+        }
+    }
+    return numbersArray[10,10];
+
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next();
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write($"{array[i,j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ pr3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Random rnd = new Random();
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] numbersArray = new double [m,n];
+FillArrayRandomNumbers(numbersArray);
+PrintArray(numbersArray);
+AverageInColumn(numbersArray);
+
+void AverageInColumn (double[,] array)
+{
+    for (int j = 0; j < numbersArray.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbersArray.GetLength(0); i++)
+    {
+        avarage = (avarage + numbersArray[i, j]);
+    }
+    avarage = avarage / n;
+    Console.WriteLine($"Среднее значение в столбце {j+1} = " + avarage + "; ");
+}
+Console.WriteLine();
+}
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next();
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write($"{array[i,j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
